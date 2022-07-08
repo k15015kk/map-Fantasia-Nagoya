eec3fc170678c5708bb7fe370cbb11cf83df5c0e
k15015kk <aitk15015kk@outlook.com>
2022-07-09 00:01:04 +0900

    データを更新

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -9,65 +9,24 @@ const Map = () => {
 
 const [jsonData, setJsonData] = useState([])
 
+  const [viewState, setViewState] = useState({
+    longitude: 139.828256420331,
+    latitude: 35.55530889289409,
+    zoom: 10,
+  });
+
   useEffect(() => {
-    const data = require("../../data/N02-20_RailroadSection.json");
-    setJsonData(data.features);
+    const data = require("../../data/LineString_Fantasia_Nagoya_2_Ebina_to_Nishi-Funabashi.json");
+    setJsonData(data);
   }, []);
 
-  const [viewState, setViewState] = useState({
-    longitude: 139.7671365841117,
-    latitude: 35.68143950212949,
-    zoom: 15,
-  });
 
   const geoJsonLayer = new GeoJsonLayer({
     id: "geojson",
     data: jsonData,
     lineWidthUnits: 'pixels',
-    getLineWidth: (data) => {
-      if (data.properties.N02_003) {
-        const n02_003 = data.properties.N02_003
-        const lastThreeWord = n02_003.slice(-3)
-
-        if (lastThreeWord === '新幹線') {
-          return 4
-        } else {
-          return 2
-        }
-
-      } else {
-        return 0
-      }
-    },
-    getLineColor: (data) => {
-      if (data.properties) {
-        switch (data.properties.N02_004) {
-          case "北海道旅客鉄道":
-            return [3, 193, 61, 224];
-            break;
-          case "東日本旅客鉄道":
-            return [55, 134, 64, 224];
-            break;
-          case "東海旅客鉄道":
-            return [255, 126, 28, 224];
-            break;
-          case "西日本旅客鉄道":
-            return [0, 114, 186, 224];
-            break;
-          case "四国旅客鉄道":
-            return [0, 172, 209, 224];
-            break;
-          case "九州旅客鉄道":
-            return [246, 46, 54, 225];
-            break;
-          default:
-            return [0, 0, 0, 0];
-            break;
-        }
-      } else {
-        return [0, 0, 0, 0];
-      }
-    },
+    getLineWidth: 4,
+    getLineColor: [255, 241, 118, 255],
   });
 
   return (
